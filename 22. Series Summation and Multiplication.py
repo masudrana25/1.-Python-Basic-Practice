c219328e15a656ce67767d3b02266f4aad0b6419
masudrana25 <mdmasudrana2512000@gmail.com>
2023-12-16 02:36:57 +0600

    dgtvcD

---
+++ 22. Series Summation and Multiplication.py
@@ -0,0 +1,105 @@
+######### Summation #########
+#### 1+2+3+4+5+.....+n =?
+
+# n = int(input('Enter the value of n : '))
+
+# i = 1;sum = 0
+# while i<=n :
+#   sum = sum +i
+#   i = i + 1
+# print(sum)
+
+#### 2+4+6+8+10+.....+n =?
+
+# n = int(input('Enter the value of n : '))
+
+# i = 2;sum = 0
+# while i<=n :
+#   sum = sum +i
+#   i = i + 2
+# print(sum)
+
+#### 1+3+5+7+9+.....+n =?
+
+# n = int(input('Enter the value of n : '))
+
+# i = 1;sum = 0
+# while i<=n :
+#   sum = sum +i
+#   i = i + 2
+# print(sum)
+
+#### 4+8+12+....+n =?
+
+# n = int(input('Enter the value of n : '))
+
+# i = 4;sum = 0
+# while i<=n :
+#   sum = sum +i
+#   i = i + 4
+# print(sum)
+
+#### 1^2+2^2+3^2+4^2+5^2+.....+n^2 =?
+
+# n = int(input('Enter the value of n : '))
+
+# i = 1;sum = 0
+# while i<=n :
+#   sum = sum +i*i
+#   i = i + 1
+# print(sum)
+
+
+#### 2^2+4^2+6^2+8^2+10^2+.....+n^2 =?
+
+# n = int(input('Enter the value of n : '))
+
+# i = 2;sum = 0
+# while i<=n :
+#   sum = sum +i*i
+#   i = i + 2
+# print(sum)
+
+#### 1+1/2+1/3+1/4+1/5+.....+1/n =?
+
+# n = int(input('Enter the value of n : '))
+
+# i = 1;sum = 0
+# while i<=n :
+#   sum = sum +1/i
+#   i = i + 1
+# print(sum)
+
+######### Multiplication #########
+
+#### 2x4x6x8x10x.....xn =?
+
+# n = int(input('Enter the value of n : '))
+
+# mul = 1
+# for x in range(2,n+1,2) :
+#   mul = mul * x
+# print(mul)
+
+#### 1x3x5x7x9x.....xn =?
+# n = int(input('Enter the value of n : '))
+# mul = 1
+# for x in range(1,n+1,2) :
+#   mul = mul * x
+# print(mul)
+
+#### 4x8x12x....xn =?
+# n = int(input('Enter the value of n : '))
+# mul =1
+# for x in range(4,n+1,4) :
+#   mul=mul*x
+# print(mul)
+
+#### 1^2x2^2x3^2x4^2x5^2x.....xn^2 =?
+# n = int(input('Enter the value of n : '))
+# mul = 1
+# for x in range(1,n+1,1) :
+#   mul = mul * x*x
+# print(mul)
+
+
---
+++ 23. Factorial .py
@@ -0,0 +1,5 @@
+a = int(input('Enter the value of n : '))
+fact = 1
+for x in range(1, a+1,1) :
+  fact = x * fact
+print(fact)
